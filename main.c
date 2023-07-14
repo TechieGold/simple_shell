6e9fb3cc740dfce97a2047669aa6ac3b072d361b
Gold Israel <techiegold101@gmail.com>
2023-07-14 02:39:30 -0700

    Add boilerplate code

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	(void)argv; (void)argv;
+
+	char *prompt = ">>";
+	
+	printf("%s\n", prompt);
+
+	return (0);
+}
