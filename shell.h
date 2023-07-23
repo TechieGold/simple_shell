3539cd0a3ed95ae257d539a8ca5d1ecd16ed44e5
Gold Israel <techiegold101@gmail.com>
2023-07-23 07:01:32 -0700

    Add header file with function prototypes

--- shell.h
+++ shell.h
@@ -1,8 +1,17 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#define MAX_ARGS 64
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+void display_prompt();
+char *read_command_line();
+void tokenize_command(char *command, char *args[]);
+void execute_command(char *command);
 
 #endif /* SHELL_H */
