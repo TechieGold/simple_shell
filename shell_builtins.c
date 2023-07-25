0cc9c8f75daa2cda612266631f5e1dd503aae837
Gold Israel <techiegold101@gmail.com>
2023-07-25 06:24:25 -0700

    Add function to handle arguments for the built-in exit

--- shell_builtins.c
+++ shell_builtins.c
@@ -10,7 +10,6 @@ void print_environment(void)
 	extern char **environ;
 	char **env = environ;
 	size_t len;
-
 	while(*env != NULL)
 	{
 		len = strlen(*env);
@@ -18,4 +17,30 @@ void print_environment(void)
 		write(STDOUT_FILENO, "\n", 1);
 		env++;
 	}
+}
+
+/**
+  * handle_exit_command - Handles exit command with or without argument.
+  * @user_input: The user's input command.
+  *
+  * Return: Returns 1 to exit the shell or the specified status to exit the shell.
+  */
+int handle_exit_command(char *user_input, int *exit_status)
+{
+	char command[MAX_COMMAND_LENGTH];
+	char *args[MAX_ARGS];
+
+	parse_command(user_input, command, args);
+
+	if (strcmp(command, "exit") == 0)
+	{
+		if (args[1] != NULL)
+		{
+			*exit_status = atoi(args[1]);
+		}
+
+		return (1);
+	}
+
+	return (0);
 }
